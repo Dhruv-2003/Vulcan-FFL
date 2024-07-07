e2a5418fe273a692a9ba63ae4fd551dc1eaa2b97
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2024-07-07 05:51:20 +0200

    feat: AVS webAPI endpoint completed for block verification

--- AVS/AVS-WebAPI/main.go
+++ AVS/AVS-WebAPI/main.go
@@ -2,27 +2,39 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"AVS-WebAPI/verifier"
 )
 
-// Define the request and response structure
-type TaskRequest struct {
+// The request and response structure
+type ValidateRequest struct {
 	ProofOfTask string `json:"proofOfTask"`
 	Performer   string `json:"performer"`
 	Data        []byte `json:"data"`
 }
 
-type TaskResponse struct {
+type ValidateResponse struct {
 	Data    bool   `json:"valid"`
 	Error   string `json:"error,omitempty"`
 	Message string `json:"message,omitempty"`
 }
 
+// The structure of the proof data from the IPFS
+type ProofData struct {
+	Block    verifier.Block `json:"block"`
+	RawState string         `json:"rawState"`
+}
+
 // Handler for the /task/validate endpoint
 func validateTaskHandler(w http.ResponseWriter, r *http.Request) {
-	var req TaskRequest
-	var res TaskResponse
+	ipfsHost := os.Getenv("IPFS_HOST")
+
+	var req ValidateRequest
+	var res ValidateResponse
 
 	// Decode the request body
 	err := json.NewDecoder(r.Body).Decode(&req)
@@ -38,16 +50,54 @@ func validateTaskHandler(w http.ResponseWriter, r *http.Request) {
 	} else {
 		// Perform validation of the execution of the block to verify
 		// 1. Take the inputs from the request as proofOfTask and data
-		// 2. Preparing the data for the block , so either fetch from the CID or something
-		// 3. Verify block execution
-		// 4. Return the result
+		cid := req.ProofOfTask
 
-		res.Data = true
+		// 2. Preparing the data for the block , so either fetch from the CID or something
+		url := fmt.Sprintf("%s/%s", ipfsHost, cid)
+
+		proofData, err := fetchUserData(url)
+		if err != nil {
+			log.Fatalf("Error fetching user data: %v", err)
+		}
+
+		currentAppState := json.RawMessage(proofData.RawState)
+
+		// 3. Verify block execution
+		valid, err := verifier.VerifyBlock(&proofData.Block, currentAppState)
+
+		// 4. Return the result
+		if err != nil {
+			res.Data = false
+			res.Error = err.Error()
+		} else {
+			res.Data = valid
+		}
 	}
 
 	// Encode the response as JSON
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(res)
+}
+
+func fetchUserData(url string) (*ProofData, error) {
+	// Perform GET request
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to perform GET request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code: %v", resp.Status)
+	}
+
+	// Decode JSON response
+	var proofData ProofData
+	if err := json.NewDecoder(resp.Body).Decode(&proofData); err != nil {
+		return nil, fmt.Errorf("failed to decode JSON response: %v", err)
+	}
+
+	return &proofData, nil
 }
 
 func main() {
