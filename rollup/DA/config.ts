5e4e97e5fc93614c51bc216704879029319bf98a
Architsharma7 <architsharma7911@gmail.com>
2024-07-08 08:07:56 +0200

    feat: added config and connect for DA

---
+++ rollup/DA/config.ts
@@ -0,0 +1,8 @@
+export default {
+    mnemonic: process.env.MNEMONIC, // The secret seed value for account used to sign transactions 
+    ApiURL: process.env.APIURL,   // Api url
+    app_id: 0,    // Application id 
+    amount: 0,    // Amount of tokens to transfer
+    receiver: "",  // Receiver address
+    endpoint: process.env.ENDPOINT,  // Endpoint
+}
